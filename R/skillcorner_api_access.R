cb462c8d3cfb0a798ee6c937d3039da222ee4723
LeonBruggemann <leon.bruggemann@skillcorner.com>
2026-06-08 10:43:49 +0200

    Include matches and data collection endpoint. Enable to choose data version for physical metrics.

--- R/skillcorner_api_access.R
+++ R/skillcorner_api_access.R
@@ -359,6 +359,209 @@ get_skc_match <- function(username,
   return(match_list)
 }
 
+#' Get SkillCorner Matches
+#'
+#' Retrieves a data frame of matches from the SkillCorner API.
+#'
+#' @param username Your SkillCorner API username.
+#' @param password Your SkillCorner API password.
+#' @param lang Language of the results. Available values: en, fr, es, de, zh_hans (default "en").
+#' @param user Filters on authorization level of user. Default value: true
+#' @param search A search term.
+#' @param team SkillCorner Team ID(s).
+#' @param stadium SkillCorner Stadium ID(s).
+#' @param competition SkillCorner Competition ID(s).
+#' @param competition_edition SkillCorner Competition Edition ID(s).
+#' @param season SkillCorner Season ID(s).
+#' @param physical_v3_last_modified__gte Filter matches where physical v3 data was last modified on or after this date.
+#' @param tracking_last_modified__gte Filter matches where tracking data was last modified on or after this date.
+#' @param extrapolated_tracking_last_modified__gte Filter matches where extrapolated tracking data was last modified on or after this date.
+#' @param extrapolated_tracking_v3_last_modified__gte Filter matches where extrapolated tracking v3 data was last modified on or after this date.
+#' @param dynamic_events_last_modified__gte Filter matches where dynamic events data was last modified on or after this date.
+#' @param dynamic_events_v2_last_modified__gte Filter matches where dynamic events v2 data was last modified on or after this date.
+#' @param in_possession_off_ball_runs_last_modified__gte Filter matches where in-possession off-ball runs data was last modified on or after this date.
+#' @param in_possession_passes_last_modified__gte Filter matches where in-possession passes data was last modified on or after this date.
+#' @param in_possession_dealing_with_pressure_last_modified__gte Filter matches where in-possession dealing with pressure data was last modified on or after this date.
+#' @param metrics_gi_ip_off_ball_runs_v2_last_modified__gte Filter matches where metrics gi ip off ball runs v2 data was last modified on or after this date.
+#' @param metrics_gi_ip_passes_v2_last_modified__gte Filter matches where metrics gi ip passes v2 data was last modified on or after this date.
+#' @param metrics_gi_ip_player_possession_v2_last_modified__gte Filter matches where metrics gi ip player possession v2 data was last modified on or after this date.
+#' @param metrics_gi_ip_passing_options_v2_last_modified__gte Filter matches where metrics gi ip passing options v2 data was last modified on or after this date.
+#' @param metrics_gi_oop_on_ball_engagements_v2_last_modified__gte Filter matches where metrics gi oop on ball engagements v2 data was last modified on or after this date.
+#' @param date_time__gte Filter matches with date_time on or after this date.
+#' @param date_time__lte Filter matches with date_time on or before this date.
+#'
+#' @return A \code{tibble} containing matches metadata.
+#' @export
+#'
+#' @importFrom purrr compact
+#' @importFrom httr modify_url GET status_code headers http_type content authenticate
+#' @importFrom jsonlite fromJSON flatten
+#' @importFrom tibble as_tibble tibble
+#' @importFrom dplyr bind_rows
+get_skc_matches <- function(username,
+                           password,
+                           search = NULL,
+                           team = NULL,
+                           stadium = NULL,
+                           competition = NULL,
+                           competition_edition = NULL,
+                           season = NULL,
+                           user = TRUE,
+                           physical_v3_last_modified__gte = NULL,
+                           tracking_last_modified__gte = NULL,
+                           extrapolated_tracking_last_modified__gte = NULL,
+                           extrapolated_tracking_v3_last_modified__gte = NULL,
+                           dynamic_events_last_modified__gte = NULL,
+                           dynamic_events_v2_last_modified__gte = NULL,
+                           in_possession_off_ball_runs_last_modified__gte = NULL,
+                           in_possession_passes_last_modified__gte = NULL,
+                           in_possession_dealing_with_pressure_last_modified__gte = NULL,
+                           metrics_gi_ip_off_ball_runs_v2_last_modified__gte = NULL,
+                           metrics_gi_ip_passes_v2_last_modified__gte = NULL,
+                           metrics_gi_ip_player_possession_v2_last_modified__gte = NULL,
+                           metrics_gi_ip_passing_options_v2_last_modified__gte = NULL,
+                           metrics_gi_oop_on_ball_engagements_v2_last_modified__gte = NULL,
+                           date_time__gte = NULL,
+                           date_time__lte = NULL,
+                           lang = "en") {
+
+  base_url <- 'https://skillcorner.com/api/matches/'
+  user_param <- if (is.logical(user)) tolower(as.character(user)) else user
+
+  query_params <- list(
+    search = search,
+    team = team,
+    stadium = stadium,
+    competition = competition,
+    competition_edition = competition_edition,
+    season = season,
+    user = user_param,
+    physical_v3_last_modified__gte = physical_v3_last_modified__gte,
+    tracking_last_modified__gte = tracking_last_modified__gte,
+    extrapolated_tracking_last_modified__gte = extrapolated_tracking_last_modified__gte,
+    extrapolated_tracking_v3_last_modified__gte = extrapolated_tracking_v3_last_modified__gte,
+    dynamic_events_last_modified__gte = dynamic_events_last_modified__gte,
+    dynamic_events_v2_last_modified__gte = dynamic_events_v2_last_modified__gte,
+    in_possession_off_ball_runs_last_modified__gte = in_possession_off_ball_runs_last_modified__gte,
+    in_possession_passes_last_modified__gte = in_possession_passes_last_modified__gte,
+    in_possession_dealing_with_pressure_last_modified__gte = in_possession_dealing_with_pressure_last_modified__gte,
+    metrics_gi_ip_off_ball_runs_v2_last_modified__gte = metrics_gi_ip_off_ball_runs_v2_last_modified__gte,
+    metrics_gi_ip_passes_v2_last_modified__gte = metrics_gi_ip_passes_v2_last_modified__gte,
+    metrics_gi_ip_player_possession_v2_last_modified__gte = metrics_gi_ip_player_possession_v2_last_modified__gte,
+    metrics_gi_ip_passing_options_v2_last_modified__gte = metrics_gi_ip_passing_options_v2_last_modified__gte,
+    metrics_gi_oop_on_ball_engagements_v2_last_modified__gte = metrics_gi_oop_on_ball_engagements_v2_last_modified__gte,
+    date_time__gte = date_time__gte,
+    date_time__lte = date_time__lte,
+    lang = lang
+  )
+  query_params <- purrr::compact(query_params)
+  current_url <- httr::modify_url(base_url, query = query_params)
+
+  pages_list <- list()
+
+  while (!is.null(current_url)) {
+    response <- httr::GET(current_url, httr::authenticate(username, password, type = "basic"))
+
+    if (httr::status_code(response) != 200) {
+      warning(paste("Error retrieving URL:", current_url, "Status Code:", httr::status_code(response)))
+      break
+    }
+
+    if (identical(httr::headers(response)$`content-type`, "application/x-gzip") ||
+        identical(httr::http_type(response), "application/octet-stream")) {
+      raw_content <- httr::content(response, as = "raw")
+      parsed_text <- memDecompress(raw_content, type = "gzip", asChar = TRUE)
+    } else {
+      parsed_text <- httr::content(response, as = "text", encoding = "UTF-8")
+    }
+
+    page_data <- tryCatch({
+      jsonlite::fromJSON(parsed_text, simplifyDataFrame = TRUE, simplifyVector = TRUE)
+    }, error = function(e) {
+      warning("Parsing JSON data failed.")
+      return(NULL)
+    })
+
+    if (is.null(page_data)) break
+
+    if (!is.null(page_data$results) && length(page_data$results) > 0) {
+      flat_page <- jsonlite::flatten(page_data$results)
+      names(flat_page) <- gsub("\\.", "_", names(flat_page))
+
+      pages_list[[length(pages_list) + 1]] <- tibble::as_tibble(flat_page)
+    }
+
+    current_url <- page_data$`next`
+  }
+
+  if (length(pages_list) == 0) {
+    warning("No matches found.")
+    return(tibble::tibble())
+  }
+
+  return(dplyr::bind_rows(pages_list))
+}
+
+#' Get SkillCorner Match Data Collection
+#'
+#' Extracts match data collection
+#'
+#' @param username Your SkillCorner API username.
+#' @param password Your SkillCorner API password.
+#' @param match_id The ID of the targeted match.
+#'
+#' @return A structural parsed \code{data.frame}.
+#' @export
+#'
+#' @importFrom purrr compact
+#' @importFrom httr modify_url GET status_code headers http_type content authenticate
+get_skc_data_collection <- function(username,
+                                    password,
+                                    match_id = NULL) {
+
+  if (is.null(match_id)) {
+    stop("Error: Please provide a valid match_id.")
+  }
+
+  base_url <- 'https://skillcorner.com/api/match/'
+  url <- paste0(base_url, match_id, "/data_collection/")
+
+  query_params <- NULL
+  query_params <- purrr::compact(query_params)
+  current_url <- httr::modify_url(url, query = query_params)
+
+  response <- httr::GET(current_url, httr::authenticate(username, password, type = "basic"))
+
+  if (httr::status_code(response) != 200) {
+    warning(paste("Error retrieving URL:", current_url, "Status Code:", httr::status_code(response)))
+    return(list())
+  }
+
+  content_type <- httr::headers(response)$`content-type`
+
+  if (is.character(content_type) && (grepl("application/x-gzip", content_type) ||
+                                     httr::http_type(response) == "application/octet-stream")) {
+    raw_content <- httr::content(response, as = "raw")
+    parsed_text <- memDecompress(raw_content, type = "gzip", asChar = TRUE)
+  } else {
+    parsed_text <- httr::content(response, as = "text", encoding = "UTF-8")
+  }
+
+  page_data <- tryCatch({
+    jsonlite::fromJSON(parsed_text, simplifyVector = TRUE)
+  }, error = function(e) {
+    warning("Parsing JSON data failed.")
+    return(list())
+  })
+
+  if (length(page_data) == 0) {
+    warning(paste("No info found for match_id", match_id, "."))
+    return(list())
+  }
+
+  return(page_data)
+}
+
 #' Get SkillCorner Physical Aggregations
 #'
 #' Loops and extracts aggregated physical outputs filtering through optional metric dimensions.
@@ -388,6 +591,7 @@ get_skc_match <- function(username,
 #' @param order_by Order performances by a metric queried. Use '-' to order in descending order. Example: '-psv99' let you order by descending PSV-99
 #' @param response_format Only JSON format allowed.
 #' @param average_per Specify the normalization we want to use to compute physical metrics. Available values: match, p90, p60bip, p30tip, p30otip.
+#' @param data_version Specify the Version of Physical Data. From July 20th 2026 on it will be Version 3.0.3 incroporating Peak Speed and Women's Threshold. For now you have to specify it.
 #'
 #' @return A consolidated \code{data.frame}.
 #' @export
@@ -420,7 +624,8 @@ get_skc_physical <- function(username,
                              group_by = NULL,
                              order_by = NULL,
                              response_format = NULL,
-                             average_per = NULL) {
+                             average_per = NULL,
+                             data_version = NULL) {
 
   base_url <- 'https://skillcorner.com/api/physical?'
 
@@ -451,7 +656,8 @@ get_skc_physical <- function(username,
     group_by = group_by,
     order_by = order_by,
     response_format = response_format,
-    average_per = average_per
+    average_per = average_per,
+    data_version = data_version
   )
 
   query_params <- lapply(query_params, function(x) {
@@ -495,7 +701,7 @@ get_skc_physical <- function(username,
   return(dplyr::bind_rows(all_results))
 }
 
-#' Get SkillCorner Dynamic Events Data
+#' Get SkillCorner Dynamic Events
 #'
 #' Extracts match dynamic events.
 #'
